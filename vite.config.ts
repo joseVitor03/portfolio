b65a761e35539e28dc25f1092de015ae6f22cd98
joseVitor03 <jv681033@gmail.com>
2024-11-21 12:42:50 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import ViteSitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    ViteSitemap({
+      siteUrl: 'https://portfolio-jose-vitor-projects.vercel.app',
+      outDir: 'public', // Onde o sitemap.xml e robots.txt serão gerados
+    }),
+  ],
 })
